f9ec26456cb2f6eaf0acf22952d25c1dd2317be9
fishagbalo <fishagbalo@gmail.com>
2024-08-19 13:17:56 +0800

    Adding Programs

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,14 @@
 import './Hero.css'
+import darkArrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        Hero
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>We Ensure better education for a better world</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, possimus exercitationem! Voluptas voluptatibus quaerat non, consectetur omnis et similique minus, fuga autem velit deleniti unde maiores fugiat iure natus.</p>
+            <button className='btn'>Explore more <img src={darkArrow} alt="" /></button>
+        </div>
         </div>
   )
 }
